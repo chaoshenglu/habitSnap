85c2a2596155457053844c53f33ef6d5d236c6b3
lixiang <271896700@qq.com>
2025-04-12 15:32:52 +0800

    2

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -343,7 +343,7 @@ async function handleLogout() {
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-bottom: 24px;
+  margin-bottom: 12px;
 }
 
 .profile-avatar {
@@ -355,6 +355,7 @@ async function handleLogout() {
   align-items: center;
   justify-content: center;
   margin-bottom: 16px;
+  margin-top: 8px;
 }
 
 .profile-email {
