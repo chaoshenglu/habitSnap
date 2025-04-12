9dc9c8c338fbf2bd3e22cec85540f596163845ea
lixiang <271896700@qq.com>
2025-04-12 12:45:43 +0800

    1

--- src/views/NewHabitView.vue
+++ src/views/NewHabitView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="new-habit-page">
     <div class="navbar">
-      <div></div>
+      <img src="/src/assets/logo.svg" style="width: 26px" />
       <h1 class="navbar-title">添加习惯记录</h1>
       <button
         @click="saveHabit"
